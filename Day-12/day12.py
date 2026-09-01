a374f02130f6a20dd08839be6d1022836e22faeb
Koushik Bodasu <149388906+KOUSHIK1B@users.noreply.github.com>
2026-09-01 16:44:22 +0530

    WHILE Loop

---
+++ Day-12/day12.py
@@ -0,0 +1,20 @@
+# Day 12 - WHILE Loop
+
+number = 1
+
+while number <= 5:
+    print(number)
+    number += 1
+
+count = 5
+while count > 0:
+    print("Countdown:", count)
+    count -= 1
+
+# While with else
+x = 1
+while x <= 3:
+    print(x)
+    x += 1
+else:
+    print("While loop completed")
