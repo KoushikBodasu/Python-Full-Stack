fd2d6bbf0ef54207b6a681286d33e051a4e3bf4f
Koushik Bodasu <149388906+KOUSHIK1B@users.noreply.github.com>
2026-09-01 16:40:07 +0530

    IF and IF-ELSE

---
+++ Day-08/day08.py
@@ -0,0 +1,14 @@
+# Day 8 - IF and IF-ELSE
+
+age = int(input("Enter your age: "))
+
+if age >= 18:
+    print("You are eligible to vote.")
+
+if age < 18:
+    print("You are not eligible to vote.")
+
+if age >= 18:
+    print("Adult")
+else:
+    print("Minor")
